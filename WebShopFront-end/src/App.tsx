1615b9c408e1ee5ba27695ec21fbd312001a67a1
Myslitel666 <tretyakov_02@bk.ru>
2023-09-29 14:31:43 +0500

    Dark Theme Added

--- WebShopFront-end/src/App.tsx
+++ WebShopFront-end/src/App.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { createTheme, ThemeProvider, CssBaseline } from '@mui/material';
 import Home from './Components/Pages/Home/Home'; // Импортируйте компонент главной страницы
+
+const theme = createTheme({
+    palette: {
+        mode: 'dark', // Устанавливаем темный режим
+        primary: {
+            main: '#90caf9', // Основной цвет вашего сайта
+        },
+        // Добавьте другие цвета и настройки по желанию
+    },
+});
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} /> {/* Маршрут для главной страницы */}
-            </Routes>
-        </Router>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} /> {/* Маршрут для главной страницы */}
+                </Routes>
+            </Router>
+        </ThemeProvider>
     );
 }
 
